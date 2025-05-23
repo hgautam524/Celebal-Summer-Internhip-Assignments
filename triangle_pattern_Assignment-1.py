18ae3f19261e1a269f2f99f3d51753d39943fab9
hgautam524 <hgautam524@gmail.com>
2025-05-23 19:27:01 +0530

    triangle_pattern_Assignment-1.py

--- triangle_pattern_Assignment-1.py
+++ triangle_pattern_Assignment-1.py
@@ -10,5 +10,21 @@ def print_triangle_pattern(rows):
 
         print( '* ' * i)
         
-        
-print_triangle_pattern(5)
+
+def upper_triangle(rows):
+    print("Upper Triangular :")
+    for i in range(rows, 0, -1):
+        print('* ' * i)
+    print()
+
+def pyramid(rows):
+    print("Pyramid :")
+    for i in range(1, rows + 1):
+        print(' ' * (rows - i) + '* ' * i)
+    print()
+
+rows = 5
+print("Lower Triangular :") 
+print_triangle_pattern(rows)
+upper_triangle(rows)
+pyramid(rows)
